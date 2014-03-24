ee53d1bd03047c948bbf760813d03709dca75467
John Tibble <john.tibble@ge.com>
2014-03-24 17:46:20 -0400

    Parameter-passing to subroutes-within-subroutes!

---
+++ sample_application/Examples/ParameterPassing/ParameterPassingInnerSubrouteController.js
@@ -0,0 +1,7 @@
+var ParameterPassingInnerSubrouteController = function(Context, StateParameters){
+	
+	Context.Model = {
+		innerSubrouteParameterExampleId: StateParameters.innerSubrouteParameterExampleId,
+		innerSubrouteParameterExampleId2: StateParameters.innerSubrouteParameterExampleId2,
+	};
+};
